b0b9a9122db7650e11fc493b61b293ec647895e5
aliu139 <arl96@cornell.edu>
2015-11-12 01:01:12 -0500

    adding to client

--- client.mli
+++ client.mli
@@ -1,4 +1,18 @@
 (*Client*)
 
+type state
+
+(*play_white allows a user to play a card*)
 val play_white: uID -> card -> unit
+
+(*judge allows a user to select the winner of a round if he is the judge*)
 val judge: uID -> card -> unit
+
+(*get_user_state returns the state of the user*)
+val get_user_state: uID -> state
+
+(*start_heatbeat creates an ivar that represents the heartbeat*)
+val start_heartbeat: uID -> 'a Async.Std.Ivar.t
+
+(*check_heartbeat returns false if the heartbeat ever fails*)
+val check_heartbeat: uID -> boolean
