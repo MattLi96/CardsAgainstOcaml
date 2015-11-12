de82af67ca6c11d580149f4d7448c7c1b1d10706
techlover10 <techlover10@gmail.com>
2015-11-11 23:17:51 -0500

    Added some descriptions

--- server.mli
+++ server.mli
@@ -2,15 +2,17 @@
 type uID
 
 (*Authentication: All functions related to user authentication*)
-(*login takes in a username and *)
+(*login takes in a username and returns a JSON containing the 
+ *corresponding uID and other related information*)
 val login: string -> json
 
-(*logout *)
+(*logout - takes a uID and logs it out of the game*)
 val logout: uID -> unit
 
-(*reset*)
+(*reset - sends a request to the server that destroys all of
+ *the user's data*)
 val reset: uID -> unit
 
-(*heartbeat*)
+(*heartbeat - takes a username and*)
 val heartbeat: uID -> unit
 val get_active: unit -> uID list
