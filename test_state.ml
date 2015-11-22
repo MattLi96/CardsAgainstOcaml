efb56247f2feff15bae6b3f900b58719ca431016
aliu139 <arl96@cornell.edu>
2015-11-22 15:56:21 -0500

    starting test file

---
+++ test_state.ml
@@ -0,0 +1,7 @@
+open State
+
+TEST_UNIT "test get_previous_wins" = ()
+TEST_UNIT "test curr_black_card" = ()
+TEST_UNIT "test scores" = ()
+TEST_UNIT "test users_played" = ()
+TEST_UNIT "test played_white_cards" = ()
