805daa623d755b5da0257c4b016358c945500c4f
Charley Chen <charleycyx@gmail.com>
2015-11-23 22:25:40 -0500

    rebuilt state and created server

--- state.ml
+++ state.ml
@@ -2,20 +2,51 @@
   type white_card = string
   type black_card = string
   type deck = BDeck of black_card list | WDeck of white_card list
+
   (*scores is represented by a list of pairs in which the first element of the
     pair is the uID and the second is the actual score)*)
   type scores = (uID * int) list
 
-  type play_state = {played : uID list}
-  type judge_state = {played : black_card list} (*played white cards*)
-  type phase_state = Play of play_state | Judge of judge_state
-
-  type state = {
-    phase : phase_state;
-    score : scores;
+  type univ_c_state = {
+    played  : (uID * white_card) list;
+    b_card  : black_card;
+    scores  : scores;
     winners : (black_card * white_card * uID);
-    b_card : black_card;
+    hand    : white_card list;
   }
+
+  type c_state = Judging of univ_c_state
+  | Playing of univ_c_state
+  | JWaiting of univ_c_state
+  | PWaiting of univ_c_state
+
+  type univ_s_state = {
+    judge  : uID;
+    played : (uID * white_card) list;
+    b_card : black_card;
+    scores : scores;
+    winners: (black_card * white_card * uID);
+
+    (*decks*)
+    b_deck : deck;
+    w_deck : deck;
+
+    (*List of (card, player) pairs matching cards played to users who played them*)
+    card_to_player : (white_card * uID) list;
+    hands          : uID * (white_card list)
+  }
+
+  type s_state = Playing of univ_s_state | Judging of univ_s_state
+
+  (*----helper methods------------------------------------------------*)
+
+  let get_univ_c : c_state -> univ_c_state = function
+    | Judging s | Playing s | PWaiting s | JWaiting s -> s
+
+  let get_univ_s : s_state -> univ_s_state = function
+    | Judging s | Playing s -> s
+
+  (*----client state methods------------------------------------------------*)
 
   (*get_previous_wins returns all of the card pairs, one white and one black,
     that have won previous rounds*)
@@ -28,26 +59,33 @@
   (* val curr_black_card: state -> black_card *)
 
   let curr_black_card current_state =
-    current_state.b_card
+    (current_state |> get_univ_c).b_card
 
   (*Method to return scores*)
   (* val scores: state -> scores *)
 
   let scores current_state =
-    current_state.score
-
-  (*----Play state methods----*)
+    (current_state |> get_univ_c).scores
 
   (*Method to return the list of users who have played in a given round*)
   (* val users_played: play_state -> uID list *)
 
-  let users_played (current_play_state: play_state): uID list =
-    current_play_state.played
-
-  (*----Judge state methods----*)
+  let users_played current_state: uID list =
+    failwith "unimplemented"
 
   (*Method to return a list of white cards played*)
   (* val played_white_cards: judge_state -> white_card list *)
 
-  let played_white_cards (current_judge_state:judge_state): white_card list =
-    current_judge_state.played
+  let played_white_cards current_state: white_card list =
+    failwith "unimplemented"
+
+  (*----server state methods------------------------------------------------*)
+
+
+  let new_state () = failwith "unimplemented"
+
+  let get_user_state state u = failwith "unimplemented"
+
+  let get_black_deck d = failwith "unimplemented"
+
+  let get_white_deck d = failwith "unimplemented"
--- state.mli
+++ state.mli
@@ -1,36 +1,75 @@
 (*Contains methods for getting various types of information in the game*)
 (* module State = sig *)
-  type uID
-  type white_card
-  type black_card
-  type deck
-  type scores
-  type play_state
-  type judge_state
-  type phase_state
+  type uID = int
+  type white_card = string
+  type black_card = string
+  type deck = BDeck of black_card list | WDeck of white_card list
 
-  type state
+  (*scores is represented by a list of pairs in which the first element of the
+    pair is the uID and the second is the actual score)*)
+  type scores = (uID * int) list
+
+  type univ_c_state = {
+    played  : (uID * white_card) list;
+    b_card  : black_card;
+    scores  : scores;
+    winners : (black_card * white_card * uID);
+    hand    : white_card list;
+  }
+
+  type c_state = Judging of univ_c_state
+  | Playing of univ_c_state
+  | JWaiting of univ_c_state
+  | PWaiting of univ_c_state
+
+  type univ_s_state = {
+    judge  : uID;
+    played : (uID * white_card) list;
+    b_card : black_card;
+    scores : scores;
+    winners: (black_card * white_card * uID);
+
+    (*decks*)
+    b_deck : deck;
+    w_deck : deck;
+
+    (*List of (card, player) pairs matching cards played to users who played them*)
+    card_to_player : (white_card * uID) list;
+    hands          : uID * (white_card list)
+  }
+
+  type s_state = Playing of univ_s_state | Judging of univ_s_state
+
+  (*----client state methods------------------------------------------------*)
 
   (*get_previous_wins returns all of the card pairs, one white and one black,
     that have won previous rounds*)
-  val get_previous_wins: state -> (black_card * white_card) list
+  val get_previous_wins: c_state -> (black_card * white_card) list
 
   (*Method to return the current black card in the state*)
-  val curr_black_card: state -> black_card
+  val curr_black_card: c_state -> black_card
 
   (*Method to return scores*)
-  val scores: state -> scores
-
-  (*----Play state methods----*)
+  val scores: c_state -> scores
 
   (*Method to return the list of users who have played in a given round*)
-  val users_played: play_state -> uID list
-
-
-  (*----Judge state methods----*)
+  val users_played: c_state -> uID list
 
   (*Method to return a list of white cards played*)
-  val played_white_cards: judge_state -> white_card list
+  val played_white_cards: c_state -> white_card list
+
+  (*----server state methods------------------------------------------------*)
+
+  (*Method for creating an initial server state*)
+  val new_state: unit -> s_state
+
+  (*Method for retrieving a user's UserState*)
+  val get_user_state: s_state -> uID -> c_state
+
+  (*Method to return the entire deck*)
+  val get_white_deck: s_state -> deck
+  val get_black_deck: s_state -> deck
+
 
 (* end *)
 
