124875b0dc3e811f44e205c9c8790e00a2c94b4e
aliu139 <arl96@cornell.edu>
2015-11-25 17:29:09 -0500

    finished play_white

--- client.ml
+++ client.ml
@@ -11,8 +11,13 @@ type state = c_state
 let play_white (uID:uID) (white:white_card) =
   let temp_header = Header.add (Header.init()) "uID" (string_of_int uID) in
   let temp_body = Body.of_string white in
-  ignore(Client.post (Uri.of_string "http://localhost:8080/") ~headers:temp_header
-    ~body:temp_body); ()
+  let post_req = (Client.post (Uri.of_string "http://localhost:8080/") ~headers:temp_header
+    ~body:temp_body) in
+  post_req >>= (fun (resp, body) ->
+  let code = resp |> Response.status |> Code.code_of_status in
+  Printf.printf "Response code: %d\n" code;
+  Printf.printf "Headers: %s\n" (resp |> Response.headers |> Header.to_string);
+  return ())
 
 (*judge allows a user to select the winner of a round if he is the judge*)
 (* val judge: uID -> white_card -> unit *)
