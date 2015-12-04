0808c1e8d3f4aef09be7f07762023fcc0be72bd5
Matthew Li <ml927@cornell.edu>
2015-12-03 22:27:22 -0500

    Changed heartbeat to use recurring call

--- heartbeat.ml
+++ heartbeat.ml
@@ -1,8 +1,8 @@
 open Model (*get type uID*)
 open Async.Std
+open RecurringCall
 
 (*
-The int is the time in seconds of a cycle
 The Ivar is for ending the cyling. When you fill the Ivar the cyling stops   
 The first list is the list of "active users"
 The second list is the list of users who have called beat since the last cycle
@@ -11,30 +11,26 @@ Every i seconds, the first list is dumped and
    the second list becomes the first list. This
    is how we keep track of the active users.
 *)
-type heartbeat = int * unit Ivar.t * uID list ref * uID list ref
+type heartbeat = unit Ivar.t * uID list ref * uID list ref
 
 (*cycle is the end of 1 heartbeat cycle. It dumps list 1 and 
   clears list 2*)
-let rec cycle hb =
-  match hb with
-  | (i, ivar, l1, l2) -> 
-    if Ivar.is_full ivar then () 
-    else (
-
-      l1 := !l2; l2 := [];
-      let _ = after (Core.Std.Time.Span.of_int_sec i) >>= 
-        (fun _ -> cycle hb; return ()) in
-      ()
-    )
+let cycle hb =
+  let (_, l1, l2) = hb in
+  l1 := !l2; l2 := []
 
 
 let create_heartbeat i = 
-  let hb = (i, Ivar.create (),ref [], ref []) in
-  cycle hb; hb
+  let ivar = Ivar.create () in
+  let hb = (ivar, ref [], ref []) in
+  let call = create_call (float_of_int i) (fun () -> cycle hb) in
+  start_call call;
+  upon (Ivar.read ivar) (fun () -> stop_call call);
+  hb
 
 let end_heartbeat hb =
   match hb with
-  | (_, ivar, _, _) -> Ivar.fill_if_empty ivar ()
+  | (ivar, _, _) -> Ivar.fill_if_empty ivar ()
 
 (*Helper function for removing duplicates*)
 let rec remove_dup lst = 
@@ -44,8 +40,10 @@ let rec remove_dup lst =
 
 let get_active_users hb = 
   match hb with
-  | (_, _, l, _) -> l := remove_dup !l; !l
+  | (_, l, _) -> !l (*l := remove_dup !l; !l*)
 
 let beat hb uid =
   match hb with
-  | (_, _, l1, l2) -> l1 := (uid::!l1);l2 := (uid::!l2)
+  | (_, l1, l2) -> l1 := (uid::!l1);l2 := (uid::!l2)
+
+
